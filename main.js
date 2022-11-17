6cf450084542440e521ea07f6487e4efb8acaf43
MohamedAliMeftah <mohamed.ali.oussaif.meftah@horizon-tech.tn>
2022-11-17 18:00:02 +0100

    finished responsive and starting work with js

--- main.js
+++ main.js
@@ -2,6 +2,34 @@
 let btnMenu = document.querySelector(".btn-mobile-nav");
 let linksBtn = document.querySelector(".header");
 
-btnMenu.addEventListener("click", (e) => {
+btnMenu.addEventListener("click", () => {
   linksBtn.classList.toggle("nav-open");
 });
+//make the links smooth
+let links = document.querySelectorAll("a:link");
+links.forEach((link) =>
+  link.addEventListener("click", (e) => {
+    //to stop propagation
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    if (href === "#") {
+      scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    }
+    if (link.classList.contains("main-nav-link")) {
+      console.log(true);
+      linksBtn.classList.toggle("nav-open");
+
+      window.location.replace(link.getAttribute("href"));
+    }
+  })
+);
